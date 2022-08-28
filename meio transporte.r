66e2fe9259c34126d646a806ac3a15fcddf249f0
sbcurso <109633406+sbcurso@users.noreply.github.com>
2022-08-28 16:11:37 -0300

    Add files via upload

---
+++ meio transporte.r
@@ -0,0 +1,42 @@
+ programa {
+    funcao inicio() {
+        cadeia resposta
+        escreva("e terrestre? ")
+        leia(resposta)
+        se (resposta == "S") {
+            escreva("cabe apenas uma pessoa? ")
+            leia(resposta)
+            se (resposta == "S") {
+                escreva("e pesado? ")
+                leia(resposta)
+                se (resposta == "S") {
+                    escreva("TRATOR")
+                }
+        	}
+        } senao {
+            escreva("nao e terrestre") 
+        } senao {
+            escreva("usa capacete? ")
+            leia(resposta
+            se (resposta == "S") }
+                escreva("MOTO")
+        } senao {
+            escreva("usa trilho? ")
+            leia(resposta)
+            se (resposta == "S") {
+                escreva("TREM")
+            } senao {
+                escreva("é alto? ")
+                leia(resposta)
+                se (resposta == "S") {
+                    
+                }
+            }
+        }
+} senao {
+    escreva("nao é terrestre")
+}
+                }
+            }
+            }
+            
